6aed680231a9f358569b17b38ad04c64bfa9700a
Alexander Jung <alex@unikraft.io>
2021-11-19 21:31:45 +0100

    modules/postgres: Introduce postgres client module
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- cmd/wayfinderd/main.go
+++ cmd/wayfinderd/main.go
@@ -47,6 +47,7 @@ import (
   _ "github.com/erda-project/erda-infra/providers/grpcclient"
   _ "github.com/erda-project/erda-infra/providers/serviceregister"
 
+  _ "github.com/unikraft/wayfinder/modules/postgres"
   _ "github.com/unikraft/wayfinder/modules/hostconfig"
 
   _ "github.com/unikraft/wayfinder/api/client"
