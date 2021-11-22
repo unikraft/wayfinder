e146c50ff51992230adf9d8890a913781bef56e8
Alexander Jung <alex@unikraft.io>
2021-11-22 16:35:19 +0100

    modules/tester,cmd/wayfinderd: Introduce tester module
    Signed-off-by: Alexander Jung <alex@unikraft.io>

---
+++ modules/tester/provider.go
@@ -0,0 +1,156 @@
+package tester
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Authors: Alexander Jung <alex@unikraft.io>
+//
+// Copyright (c) 2021, Unikraft UG.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+import (
+  "fmt"
+  "net"
+  "time"
+  "reflect"
+  "encoding/json"
+
+  "github.com/erda-project/erda-infra/pkg/transport"
+  "github.com/erda-project/erda-infra/base/servicehub"
+  
+  "github.com/unikraft/wayfinder/api/proto"
+  "github.com/erda-project/erda-infra/base/logs"
+  "github.com/unikraft/wayfinder/modules/libvirt"
+  "github.com/unikraft/wayfinder/modules/postgres"
+  "github.com/unikraft/wayfinder/modules/container"
+)
+
+type config struct {
+  DefaultStartDelay      time.Duration `file:"default_start_delay"      env:"TESTER_DEFAULT_START_DELAY"      default:"5s"`
+  MetricsFreq            time.Duration `file:"metrics_freq"             env:"TESTER_METRICS_FREQ"             default:"1s"`
+  MetricsReceiverType    string        `file:"metrics_receiver_type"    env:"TESTER_METRICS_RECEIVER_TYPE"    default:"tcp"`
+  MetricsReceiver        string        `file:"metrics_receiver"         env:"TESTER_METRICS_RECEIVER"         default:"127.0.0.1:12345"`
+  MetricsReceiverTimeout time.Duration `file:"metrics_receiver_timeout" env:"TESTER_METRICS_RECEIVER_TIMEOUT" default:"5s"` 
+}
+
+type provider struct {
+  Cfg          *config
+  Log           logs.Logger
+  Register      transport.Register
+  service      *Service
+  DB            postgres.Interface `autowired:"postgres"`
+  Container    *container.Service  `autowired:"container"`
+  Libvirt      *libvirt.Service    `autowired:"libvirt"`
+  // metricServer  net.Conn
+}
+
+func (p *provider) Init(ctx servicehub.Context) error {
+  // TODO: This connection should be checked and re-established if it fails
+  // (e.g. if the remote receiver is restarted)
+  // conn, err := net.Dial(p.Cfg.MetricsReceiverType, p.Cfg.MetricsReceiver)
+  // if err != nil {
+  //   return fmt.Errorf("failed to open metrics server connection to %s://%s", p.Cfg.MetricsReceiverType, p.Cfg.MetricsReceiver)
+  // }
+  
+  // if p.Cfg.MetricsReceiverTimeout > 0 {
+  //   conn.SetDeadline
+  // }Z
+
+  // p.Log.Infof("connected to metrics server: %s://%s", p.Cfg.MetricsReceiverType, p.Cfg.MetricsReceiver)
+  // p.metricServer = conn
+
+  p.service = &Service{
+    p:     p,
+    tests: make(map[string]*test),
+  }
+
+  if p.Register != nil {
+    proto.RegisterTesterServiceImp(p.Register, p.service)
+  }
+
+  return nil
+}
+
+func (p *provider) Provide(ctx servicehub.DependencyContext, args ...interface{}) interface{} {
+  switch {
+    case ctx.Service() == "tester",
+         ctx.Service() == "wayfinder.TesterService",
+         ctx.Type() == proto.TesterServiceServerType(),
+         ctx.Type() == proto.TesterServiceHandlerType():
+      return p.service
+  }
+
+  return p
+}
+
+func (p *provider) PushMetrics(testUuid string, metrics map[string]string) error {
+  metrics["test_uuid"] = testUuid
+
+  // TODO: This is a bit ugly
+  jsonBytes, err := json.Marshal(map[string]map[string]string{
+    "domain": metrics,
+  })
+  if err != nil {
+    return fmt.Errorf("could not serialize metrics: %s", err)
+  }
+
+  conn, err := net.Dial(p.Cfg.MetricsReceiverType, p.Cfg.MetricsReceiver)
+  if err != nil {
+    return fmt.Errorf("failed to open metrics server connection to %s://%s", p.Cfg.MetricsReceiverType, p.Cfg.MetricsReceiver)
+  }
+
+  defer conn.Close()
+
+  // _, err = p.metricServer.Write(jsonBytes)
+  _, err = conn.Write(jsonBytes)
+  if err != nil {
+    return fmt.Errorf("failed to send to network connection: %s", err)
+  }
+
+  return nil
+}
+
+func init() {
+  servicehub.Register("tester", &servicehub.Spec{
+    Services:             []string{
+      "tester",
+      "wayfinder.TesterService",
+    },
+    Types:                []reflect.Type{},
+    Dependencies:         []string{
+      "postgres",
+    },
+    OptionalDependencies: []string{
+      "service-register",
+    },
+    Description:            "",
+    ConfigFunc:             func() interface{} {
+      return &config{}
+    },
+    Creator:                func() servicehub.Provider {
+      return &provider{}
+    },
+  })
+}
---
+++ modules/tester/service.go
@@ -0,0 +1,464 @@
+package tester
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Authors: Alexander Jung <alex@unikraft.io>
+//
+// Copyright (c) 2021, Unikraft UG.  All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+import (
+  "fmt"
+  "time"
+  "sync"
+  "context"
+
+  "google.golang.org/grpc/codes"
+  "google.golang.org/grpc/status"
+
+  "github.com/unikraft/wayfinder/api/proto"
+  "github.com/unikraft/wayfinder/modules/libvirt"
+  "github.com/unikraft/wayfinder/internal/models"
+  "github.com/unikraft/wayfinder/modules/container"
+  "github.com/unikraft/wayfinder/pkg/common/errors"
+)
+
+type test struct {
+  sync.RWMutex
+  domain       *libvirt.Domain
+  startDelay    time.Duration
+  container    *container.Container
+  err           error
+  runtime       time.Duration
+}
+
+var (
+  pidCount   = 0
+  // This variable contains a map of fake PIDs and a corresponding UUID for a
+  // test and can be used to uniquely assign a numerical value to UUID.
+  pidUuidMap = make(map[int]string, 0)
+)
+
+type Service struct {
+  p         *provider
+  tests      map[string]*test
+}
+
+func (s *Service) CreateTest(ctx context.Context, req *proto.CreateTestRequest) (*proto.CreateTestResponse, error) {
+  if req.PermutationId <= 0 {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("permutation_id")
+  }
+    
+    // TODO: Auto-populate kernel + benchtool from config if not provided.
+  if req.Kernel == nil {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("kernel")
+  }
+
+  if req.Kernel.Image == "" {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("kernel.image")
+  }
+
+  if len(req.Kernel.Cores) <= 0 {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("kernel.cores")
+  }
+
+  if req.Kernel.Memory == "" {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("kernel.memory")
+  }
+
+  if req.BenchTool == nil {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("benchtool")
+  }
+
+  if req.BenchTool.Image == "" {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("benchtool.image")
+  }
+
+  if req.BenchTool.Commands == "" {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("benchtool.commands")
+  }
+
+  if len(req.BenchTool.Cores) == 0 {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("benchtool.cores")
+  }
+
+  // Create a new entry in the database for this test
+  testModel, err := s.p.DB.Repos().Tests().CreateTestForPermutation(uint(req.PermutationId))
+  if err != nil {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "could not create test in database: %s", err)
+  }
+
+  uuid := testModel.UUID.String()
+
+  if _, ok := s.tests[uuid]; ok {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "test with uuid=%s already exists", uuid)
+  }
+
+  // Create a new fake PID to be used for the bridge and for counting the
+  // domains
+  pidCount += 1
+  pid := pidCount
+
+  pidUuidMap[pid] = uuid
+  
+  domain, err := s.p.Libvirt.NewDomain(
+    pid,
+    uuid,
+    req.Kernel.Image,
+    req.Kernel.InitRd,
+    req.Kernel.Args,
+    req.Kernel.Cores,
+  )
+  if err != nil {
+    s.p.DB.Repos().Tests().SetStatusKernelFailedByTestUuid(uuid)
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "cannot create test VM: %s", err)
+  }
+
+  // Initialize the domain
+  if err = domain.Init(); err != nil {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "could not get initialise domain: %s", err)
+  }
+
+  // Initialize the benchmark tool's container
+  container, err := s.p.Container.NewContainer(uuid)
+  if err != nil {
+    s.p.DB.Repos().Tests().SetStatusBenchToolFailedByTestUuid(uuid)
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "cannot create test container: %s", err)
+  }
+  
+  if err := container.PullAndAttachImage(req.BenchTool.Image); err != nil {
+    s.p.DB.Repos().Tests().SetStatusBenchToolFailedByTestUuid(uuid)
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "cannot pull test container's image: %s", err)
+  }
+
+  if err := container.SetCommands(req.BenchTool.Commands); err != nil {
+    s.p.DB.Repos().Tests().SetStatusBenchToolFailedByTestUuid(uuid)
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "cannot set test container's command: %s", err)
+  }
+
+  var envVars []string
+
+  if domain.IP() == nil {
+    return &proto.CreateTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "could not get domain IP: %s", err)
+  }
+
+  envVars = append(envVars, fmt.Sprintf("WAYFINDER_DOMAIN_IP_ADDR=%s", domain.IP().String()))
+
+  if len(req.BenchTool.EnvVars) > 0 {
+    for _, env := range req.BenchTool.EnvVars {
+      envVars = append(envVars, fmt.Sprintf("%s=%s", env.Name, env.Value))
+    }
+  }
+
+  envVars = append(envVars, fmt.Sprintf("WAYFINDER_TOTAL_CORES=%d", len(req.BenchTool.Cores)))
+
+  for i, core := range req.BenchTool.Cores {
+    envVars = append(envVars, fmt.Sprintf("WAYFINDER_CORE_ID%d=%d", i, core))
+  }
+
+  container.AddEnvVars(envVars)
+  container.SetCores(req.BenchTool.Cores)
+
+  if len(req.BenchTool.Devices) > 0 {
+    container.SetDevices(req.BenchTool.Devices)
+  }
+
+  if len(req.BenchTool.Capabilities) > 0 {
+    container.SetCapabilities(req.BenchTool.Capabilities)
+  }
+
+  startDelay := s.p.Cfg.DefaultStartDelay
+  if req.BenchTool.StartDelay > 0 {
+    // TODO: Update spec to use time.Duration
+    startDelay = time.Second * time.Duration(req.BenchTool.StartDelay)
+  }
+
+  // Save the build
+  s.tests[uuid] = &test{
+    domain:     domain,
+    container:  container,
+    startDelay: startDelay,
+  }
+
+  return &proto.CreateTestResponse{
+    Success: true,
+    Uuid:    uuid,
+  }, nil
+}
+
+func (s *Service) StartTest(ctx context.Context, req *proto.StartTestRequest) (*proto.StartTestResponse, error) {
+  if req.Uuid == "" {
+    return &proto.StartTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("uuid")
+  }
+
+  // TODO: Look up tests with jobId/permId rather than via container ID (this
+  // is for the proto/API).
+
+  test, ok := s.tests[req.Uuid]
+  if !ok {
+    return &proto.StartTestResponse{
+      Success: false,
+    }, status.Errorf(codes.NotFound, "cannot find test with id=%s", req.Uuid)
+  }
+
+  err := test.container.Init()
+  if err != nil {
+    test.err = err
+    s.p.DB.Repos().Tests().SetStatusBenchToolFailedByTestUuid(req.Uuid)
+
+    return &proto.StartTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "could not initialize test container: %s", err)
+  }
+
+  s.p.DB.Repos().Tests().SetStatusRunningByTestUuid(req.Uuid)
+
+  // Start a thread which oversees the running VM domain
+  go func(){
+    // Start the domain
+    err := test.domain.Start()
+    if err != nil {
+      test.err = err
+      s.p.DB.Repos().Tests().SetStatusKernelFailedByTestUuid(req.Uuid)
+    }
+  }()
+
+  // Start a threat which oversees the running benchmark tool's container
+  go func(){
+    // Give the container a delay in its startup, so we can wait for kernel
+    // to boot up correctly
+    s.p.Log.Debugf("Waiting %d seconds before starting benchtool...", test.startDelay)
+    time.Sleep(time.Duration(test.startDelay) * time.Second)
+
+    pushMetrics := true
+
+    // Start measuring the domain's resources, after the start delay
+    // NOTE: The benchtool may have a warm up time too, the resources should
+    // only be started to measure after this warm up time.  How do we calculate
+    // the warm up time? Is it a spec option?
+    // TODO: Nested threading...
+    go func() {
+      for pushMetrics {
+        time.Sleep(s.p.Cfg.MetricsFreq)
+        if err := s.p.PushMetrics(req.Uuid, test.domain.GetResourceMeasurements()); err != nil {
+          s.p.Log.Errorf("could not push metrics: %s", err)
+        }
+      }
+    }()
+
+    // Now start the container.  On return, the benchtool has exited.
+    runtime, err := test.container.StartAndWait()
+    pushMetrics = false
+
+    test.RLock()
+    defer test.RUnlock()
+    
+    // Save the total runtime of the benchtool and thus the experiment.
+    test.runtime = runtime
+    s.p.DB.Repos().Tests().SetRuntimeByTestUuid(req.Uuid, runtime)
+
+    if err != nil {
+      test.err = err
+      s.p.DB.Repos().Tests().SetStatusBenchToolFailedByTestUuid(req.Uuid)
+    } else {
+      s.p.DB.Repos().Tests().SetStatusSuccessByTestUuid(req.Uuid)
+
+      // Save all the known results from the test
+      if len(req.Results) > 0 {
+        for _, result := range req.Results {
+          switch result.Type {
+            case proto.TestResultType_TEST_RESULT_INT:
+              value, err := test.container.GetResultInt(result.Path)
+              if err != nil {
+                s.p.Log.Errorf("could not get test result value: %s", err)
+                continue
+              }
+
+              _, err = s.p.DB.Repos().Results().SaveResultIntByTestUuid(req.Uuid, result.Name, value)
+              if err != nil {
+                s.p.Log.Errorf("could not save test result: %s", err)
+              }
+
+            case proto.TestResultType_TEST_RESULT_STR:
+              value, err := test.container.GetResultStr(result.Path)
+              if err != nil {
+                s.p.Log.Errorf("could not get test result value: %s", err)
+                continue
+              }
+
+              _, err = s.p.DB.Repos().Results().SaveResultStrByTestUuid(req.Uuid, result.Name, value)
+              if err != nil {
+                s.p.Log.Errorf("could not save test result: %s", err)
+              }
+
+            case proto.TestResultType_TEST_RESULT_FLOAT:
+              value, err := test.container.GetResultFloat(result.Path)
+              if err != nil {
+                s.p.Log.Errorf("could not get test result value: %s", err)
+                continue
+              }
+
+              _, err = s.p.DB.Repos().Results().SaveResultFloatByTestUuid(req.Uuid, result.Name, value)
+              if err != nil {
+                s.p.Log.Errorf("could not save test result: %s", err)
+              }
+
+            case proto.TestResultType_TEST_RESULT_BOOL:
+              value, err := test.container.GetResultBool(result.Path)
+              if err != nil {
+                s.p.Log.Errorf("could not get test result value: %s", err)
+                continue
+              }
+
+              _, err = s.p.DB.Repos().Results().SaveResultBoolByTestUuid(req.Uuid, result.Name, value)
+              if err != nil {
+                s.p.Log.Errorf("could not save test result: %s", err)
+              }
+          }
+        }
+      }
+    }
+  }()
+
+  return &proto.StartTestResponse{
+    Success: true,
+  }, nil
+}
+
+func (s *Service) GetTestStatus(ctx context.Context, req *proto.GetTestStatusRequest) (*proto.GetTestStatusResponse, error) {
+  if req.Uuid == "" {
+    return &proto.GetTestStatusResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("uuid")
+  }
+
+  testModel := &models.Test{}
+  if err := s.p.DB.DB().Where("uuid = ?", req.Uuid).First(&testModel).Error; err != nil {
+    return &proto.GetTestStatusResponse{
+      Success: false,
+    }, status.Errorf(codes.NotFound, "test with uuid=%s not found", req.Uuid)
+  }
+
+  // TODO: Look up tests with jobId/permId rather than via test ID (this
+  // is for the proto/API).
+
+  test, ok := s.tests[req.Uuid]
+  if !ok {
+    return &proto.GetTestStatusResponse{
+      Success: false,
+    }, status.Errorf(codes.NotFound, "cannot find test with uuid=%s", req.Uuid)
+  }
+
+  return &proto.GetTestStatusResponse{
+    Success: true,
+    Status:  testModel.Status,
+    Runtime: test.runtime.String(),
+  }, nil
+}
+
+func (s *Service) DestroyTest(ctx context.Context, req *proto.DestroyTestRequest) (*proto.DestroyTestResponse, error) {
+  if req.Uuid == "" {
+    return &proto.DestroyTestResponse{
+      Success: false,
+    }, errors.NewMissingParameterError("uuid")
+  }
+
+  // TODO: Look up builds with jobId/permId rather than via container ID (this
+  // is for the proto/API).
+
+  test, ok := s.tests[req.Uuid]
+  if !ok {
+    return &proto.DestroyTestResponse{
+      Success: false,
+    }, status.Errorf(codes.NotFound, "cannot find test with uuid=%s", req.Uuid)
+  }
+  
+  // TODO: Actually we should try and garbage collect this later too.
+
+  // var err error
+  // // Try 3 times to kill domain
+  // for i := 3; i > 0; i-- {
+  //   fmt.Printf("Trying to kill domain...\n")
+    
+  //   time.Sleep(1 * time.Second)
+  // }
+
+  if err := test.domain.Destroy(); err != nil {
+    return &proto.DestroyTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "cannot destroy test kernel domain: %s", err)
+  }
+  
+  if err := test.container.Destroy(); err != nil {
+    return &proto.DestroyTestResponse{
+      Success: false,
+    }, status.Errorf(codes.Internal, "cannot destroy test benchtool container: %s", err)
+  }
+
+  // wipe the test from memory
+  delete(pidUuidMap, test.domain.FakePid())
+  delete(s.tests, req.Uuid)
+
+  return &proto.DestroyTestResponse{
+    Success: true,
+  }, nil
+}
