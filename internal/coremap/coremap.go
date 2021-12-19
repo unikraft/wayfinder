162c8db20b982d1adfca9cd95ec54e064a8cc52c
Alexander Jung <alex@unikraft.io>
2021-12-19 22:18:19 +0100

    internal/coremap: Fix searching for cores by ID
    This commit fixes the generic function to serach for a Core by its
    ID.  The function returned early during the loop through all of the
    NUMA nodes, meaning it would only search the first NUMA node.  In
    many cases, this would mean the core was not found if there is more
    than one node.
    
    Signed-off-by: Alexander Jung <alex@unikraft.io>

--- internal/coremap/coremap.go
+++ internal/coremap/coremap.go
@@ -195,11 +195,16 @@ func (cm *CoreMap) FindCoreOnNumaNode(coreId, numaNodeId uint64) (*Core, error) 
 
 // Find a core based on its ID
 func (cm *CoreMap) FindCore(coreId uint64) (*Core, error) {
+  var core *Core
   for numaId, _ := range cm.numaNodes {
-    return cm.FindCoreOnNumaNode(coreId, numaId)
+    core, _ = cm.FindCoreOnNumaNode(coreId, numaId)
   }
 
-  return nil, fmt.Errorf("could not find core with id=%d", coreId)
+  if core == nil {
+    return nil, fmt.Errorf("could not find core with id=%d", coreId)
+  }
+
+  return core, nil
 }
 
 // Set the activity of a core on a NUMA node
