4c957c4af8f9abe34a24c6d9ab6f4c74afce82b5
Haris Rotsos <c.rotsos@lancaster.ac.uk>
2022-02-09 15:38:27 +0000

    Correct run commands

--- examples/lupine-nginx/run.sh
+++ examples/lupine-nginx/run.sh
@@ -1,5 +1,2 @@
-docker run -d -p -v .:/mnt --name lupine-build lupine-nginx
-docker exec -ti lupine-build sh /mnt/build.sh
-docker rm lupine-build
-docker run  -v ~/wayfinder/examples/lupine-nginx/:/mnt/ --privileged -ti wayfinder/lupine 
 docker build -f Dockerfile  -t wayfinder/lupine:latest . 
+docker run  -v ~/wayfinder/examples/lupine-nginx/:/mnt/ --privileged -ti wayfinder/lupine sh /mnt/build.sh  
