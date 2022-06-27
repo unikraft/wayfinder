6778abe07cec68bc6f15db1dda609b8c5b0135a6
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2022-06-27 14:12:29 +0300

    scripts: Remove Minio dependency
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- scripts/wayfinder.sh
+++ scripts/wayfinder.sh
@@ -8,8 +8,7 @@ clear;
 
 POSTGRES_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wayfinder-postgres)
 REDIS_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wayfinder-redis)
-MINIO_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wayfinder-minio)
 
 clear;
 
-MINIO_ENDPOINT="${MINIO_IP}:9000" POSTGRES_HOST="${POSTGRES_IP}" REDIS_ADDR="${REDIS_IP}:6379" ./dist/wayfinderd -c ./config/wayfinderd.yaml
+POSTGRES_HOST="${POSTGRES_IP}" REDIS_ADDR="${REDIS_IP}:6379" ./dist/wayfinderd -c ./config/wayfinderd.yaml
