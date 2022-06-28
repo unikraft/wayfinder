7bab4789d9338d27c84a9c47a0f98bcb29527131
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2022-06-28 13:59:19 +0300

    scripts: Remove sudo prefix for commands
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- scripts/resetrun.sh
+++ scripts/resetrun.sh
@@ -6,10 +6,11 @@ UUID=$(virsh list | grep running | awk '{ print $2 }')
 virsh destroy $ID
 virsh undefine $UUID
 
-INT_TO_UNIQUE=$(sudo ip link show | grep -ho "ethc[0-9]*")
+INT_TO_UNIQUE=$(ip link show | grep -ho "ethc[0-9]*")
 INT_TO_CLEAR=$(echo -e "${INT_TO_UNIQUE// /\\n}" | sort -u)
 
 for interface in $INT_TO_CLEAR; do
-    sudo ip link set $interface down
-    sudo ip link delete $interface
+    ip link set $interface down
+    ip link delete $interface
+    echo "Interface $interface deleted"
 done
