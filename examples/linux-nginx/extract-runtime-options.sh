df9e2ec83e9458c37b9d91e96863605cd3bb085a
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2022-07-15 12:41:11 +0300

    examples/linux-nginx: Find ranges for parameters
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

--- examples/linux-nginx/extract-runtime-options.sh
+++ examples/linux-nginx/extract-runtime-options.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Extract runtime options from /proc and /sys
-# The script formats a list of parameters taken from /proc
+# The  formats a list of parameters taken from /proc
 # and prints it in the yaml format.
 
 number_check_re='^[0-9]+$'
@@ -13,24 +13,35 @@ fi
 
 # Filter only files that are writable
 for option in `find $1 -maxdepth 1 -type f -perm /222 -printf "%f\n"`; do
-	default_val=`cat $1/$option`
-	echo "- name:    $option"
-	echo "# path:    $1/$option"
+  default_val=`cat $1/$option`
+  echo "- name:    $option"
+  echo "# path:    $1/$option"
 
-	if [[ $default_val == "0" || $default_val == "1" ]]; then
-		echo "  type:    string"
+  if [[ $default_val == "0" || $default_val == "1" ]]; then
+    echo "  type:    string"
     echo "  default: \"$default_val\""
     echo "  choices: [\"0\", \"1\"]"
   elif [[ $default_val =~ $number_check_re ]]; then
-		echo "  type:    int"
+    max_val=$default_val
+    min_val=$default_val
+    while echo "$max_val" > $1/$option; res=$?; [[ $res -eq 0 && $max_val -le 2147483647 ]]; do max_val=$(($max_val*10)); done
+    if [[ $res -ne 0 ]]; then max_val=$(($max_val/10)); fi
+    while echo "$min_val" > $1/$option; res=$?; [[ $res -eq 0 && $min_val -gt 0 ]]; do min_val=$(($min_val/10)); done
+    if [[ $res -ne 0 ]]; then min_val=$(($min_val*10)); fi
+    if [[ $res -ne 0 && $min_val -eq 0 && $default_val -ge 10 ]]; then min_val=10; fi
+    if [[ $res -ne 0 && $min_val -eq 0 && $default_val -le 9 ]]; then min_val=$default_val; fi
+    echo -n "$default_val" > $1/$option
+    echo "  type:    int"
+    echo "  min:     $min_val"
+    echo "  max:     $max_val"
     echo "  default: $default_val"
     echo "  only:    [$default_val]"
   else
-		echo "  type:    string"
+    echo "  type:    string"
     echo "  default: \"$default_val\""
     echo "  only:    [\"$default_val\"]"
-	fi
+  fi
 
   echo "  when:    test"
-	echo ""
+  echo ""
 done
