96463f332b1706bcc70f995a9ae3451cace7bb97
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2022-07-27 16:59:18 +0300

    support/sqlite-benchtool: Add benchtool to wait for results
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@gmail.com>

---
+++ support/sqlite-benchtool/test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Usage: test.sh
+
+if [[ -z "${WAYFINDER_CORE_ID0}" ]]; then
+  echo "Missing core to pin!"
+  exit 1
+fi
+
+if [[ -z "${WAYFINDER_DOMAIN_IP_ADDR}" ]]; then
+  echo "Missing domain IP address!"
+  exit 1
+fi
+
+ping -c 1 ${WAYFINDER_DOMAIN_IP_ADDR} > /dev/null 2>&1
+if [[ $? -ne 0 ]]; then
+  echo "Domain IP address is not reachable!"
+  exit 1
+fi
+
+echo "Waiting for results..."
+
+set -xe
+
+curl --http0.9 -s ${WAYFINDER_DOMAIN_IP_ADDR}:8080 > /results.txt
+
+if [[ ! -f /results.txt ]]; then
+  echo "No results!"
+  exit 1
+fi
+
+mkdir -p /results/
+echo -n "$(cat /results.txt | awk '{ print $3 }')" > /results/benchmark.txt
+
+echo "Done!"
