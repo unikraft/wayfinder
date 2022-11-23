027c42decdc609f0ace986ec52a566f84df333ab
Nikolaos Karaolidis <nick@karaolidis.com>
2022-11-23 09:05:44 +0000

    scripts: Update wayfinder start script
    Signed-off-by: Nikolaos Karaolidis <nick@karaolidis.com>

--- scripts/wayfinder.sh
+++ scripts/wayfinder.sh
@@ -2,13 +2,7 @@
 
 set -xe
 
-clear;
-
-# make wayfinderd
-
 POSTGRES_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wayfinder-postgres)
 REDIS_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' wayfinder-redis)
 
-clear;
-
-POSTGRES_HOST="${POSTGRES_IP}" REDIS_ADDR="${REDIS_IP}:6379" ./dist/wayfinderd -c ./config/wayfinderd.yaml
+POSTGRES_HOST="${POSTGRES_IP}" REDIS_ADDR="${REDIS_IP}:6379" ./dist/wayfinderd -c ./config/wayfinderd.yaml "$@"
