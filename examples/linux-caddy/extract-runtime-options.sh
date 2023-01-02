b21de6ba9e0be366b3146f7296df6ca199e52a08
Nikolaos Karaolidis <nick@karaolidis.com>
2023-01-02 13:58:48 +0200

    examples/linux-caddy: Add Caddy Linux example
    Signed-off-by: Nikolaos Karaolidis <nick@karaolidis.com>

---
+++ examples/linux-caddy/extract-runtime-options.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+## Extract runtime options from /proc and /sys
+# The  formats a list of parameters taken from /proc
+# and prints it in the yaml format.
+
+number_check_re='^[0-9]+$'
+
+if [ ! $# == "1" ]; then
+	echo "usage: $0 /proc/sys/path/to/subfolder"
+	exit
+fi
+
+# Filter only files that are writable
+for option in `find $1 -maxdepth 1 -type f -perm /222 -printf "%f\n"`; do
+  default_val=`cat $1/$option`
+  echo "- name:    $option"
+  echo "# path:    $1/$option"
+
+  if [[ $default_val == "0" || $default_val == "1" ]]; then
+    echo "  type:    string"
+    echo "  default: \"$default_val\""
+    echo "  choices: [\"0\", \"1\"]"
+  elif [[ $default_val =~ $number_check_re ]]; then
+    max_val=$default_val
+    min_val=$default_val
+    while echo "$max_val" > $1/$option; res=$?; [[ $res -eq 0 && $max_val -le 2147483647 ]]; do max_val=$(($max_val*10)); done
+    if [[ $res -ne 0 ]]; then max_val=$(($max_val/10)); fi
+    while echo "$min_val" > $1/$option; res=$?; [[ $res -eq 0 && $min_val -gt 0 ]]; do min_val=$(($min_val/10)); done
+    if [[ $res -ne 0 ]]; then min_val=$(($min_val*10)); fi
+    if [[ $res -ne 0 && $min_val -eq 0 && $default_val -ge 10 ]]; then min_val=10; fi
+    if [[ $res -ne 0 && $min_val -eq 0 && $default_val -le 9 ]]; then min_val=$default_val; fi
+    echo -n "$default_val" > $1/$option
+    echo "  type:    int"
+    echo "  min:     $min_val"
+    echo "  max:     $max_val"
+    echo "  default: $default_val"
+    echo "  only:    [$default_val]"
+  else
+    echo "  type:    string"
+    echo "  default: \"$default_val\""
+    echo "  only:    [\"$default_val\"]"
+  fi
+
+  echo "  when:    test"
+  echo ""
+done
